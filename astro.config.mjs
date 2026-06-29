537269eba0840ecad4d3705badab4ac9832df5e8
barry burton <barrycburton@me.com>
2026-06-29 10:40:03 -0700

    Fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,12 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { excerpt } from './src/remark.mjs'
-import react from '@astrojs/react';
 import markdoc from '@astrojs/markdoc';
-import keystatic from '@keystatic/astro'
 
 export default defineConfig({
   site: 'https://example.com',
   trailingSlash: 'ignore',
-  integrations: [mdx(), sitemap(), react(), markdoc(), keystatic()],
+  integrations: [mdx(), sitemap(), markdoc()],
   markdown: {
     remarkPlugins: [excerpt],
   },
